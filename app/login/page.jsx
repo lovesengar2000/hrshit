b89371b67b26bbb5af60663336e4171162aafe70
Mamokoru <36095681+Mamokoru@users.noreply.github.com>
2026-02-22 00:59:03 +0530

    Login Page Added

---
+++ app/login/page.jsx
@@ -0,0 +1,56 @@
+'use client'
+import React from "react";
+import style from "./login.module.css";
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const page = () => {
+  const router = useRouter(); 
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const [IsExsistingUser, setIsExsistingUser] = useState(null);
+
+  const FocusChanged = async (e) => {
+    const email = e.target.value;
+    const isValid = emailRegex.test(email);
+    if(isValid){
+      // Call API to check if user exists
+      // Set to true if user exists, false otherwise
+    }
+    console.log(email);
+  }
+  const OnForgotPasswordClicked = () => {
+    router.push('/forgotPassword');
+  }
+  return (
+    <div className={style.LoginWrapper}>
+      <div className={style.LoginModal}>
+        <div className={style.FormBlock}>
+          <input
+            id="EmailField"
+            placeholder="Email"
+            type="text"
+            className={style.Input}
+            onBlur={(e)=> FocusChanged(e)}
+          />
+          {IsExsistingUser !== null && 
+             <input
+              style={{display: IsExsistingUser ? 'block' : 'none'}}
+              id="PasswordField"
+              placeholder="Password"
+              type="password"
+              className={style.Input}
+            />
+          }
+          {IsExsistingUser !== null && !IsExsistingUser && <div className={style.ForgotPassword}>Forgot password ?</div>}
+
+          <button className={style.LoginBtn}>Login</button>
+
+          <div onClick={()=> OnForgotPasswordClicked()} className={style.ForgotPassword}>Forgot password ?</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
